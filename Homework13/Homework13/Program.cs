dca3eba9c9fe56bbe4a0c53dd10cd8360ca13838
Viggo Trabalik <vvtrabalik@gmail.com>
2025-01-15 21:15:48 +0100

    Homework12Copy

--- Homework13/Homework13/Program.cs
+++ Homework13/Homework13/Program.cs
@@ -1,15 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Text.Json;
 
-namespace Homework13
+namespace Homework12
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            await GetPublicApiDataAsync();
+        }
+
+        public static async Task GetPublicApiDataAsync()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+
+                const string apiKey = "6c9325335c464251b44121921242512";
+                const string city = "New York";
+                string url = $"http://api.weatherapi.com/v1/current.json?key={apiKey}&q={city}&aqi=no";
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine("Http response message:");
+                Console.WriteLine(responseBody);
+
+                DeserializeJsonDemo(responseBody);
+
+                client.Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+        public static void DeserializeJsonDemo(string responseBody)
+        {
+            var weatherData = JsonSerializer.Deserialize<WeatherResponse>(responseBody);
+
+            if (weatherData != null && weatherData.current != null)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Weather Data for {weatherData.location.name}.");
+                Console.WriteLine($"Temperature: {weatherData.current.tempc}°C.");
+                Console.WriteLine($"Wind Speed: {weatherData.current.windkph} km/h.");
+            }
+            else
+            {
+                Console.WriteLine("Error when deserializing.");
+            }
         }
     }
 }
---
+++ Homework13/Homework13/WeatherResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework12
+{
+    public class WeatherResponse
+    {
+        public Location location { get; set; }
+        public CurrentWeather current { get; set; }
+    }
+
+    public class Location
+    {
+        public string name { get; set; }
+        public string country { get; set; }
+    }
+
+    public class CurrentWeather
+    {
+        public float tempc { get; set; }
+        public float windkph { get; set; }
+
+    }
+}
